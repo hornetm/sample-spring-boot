893fe74dbddb77520e6e5030408dd39aee3a3f24
inow <mvincent@sigma.fr>
2018-03-21 16:56:18 +0100

    new file:   .classpath 	new file:   .gitignore 	new file:   .project 	new file:   bin/com/example/DemoApplication.class 	new file:   bin/com/example/DemoApplicationTests.class 	modified:   src/main/java/com/example/DemoApplication.java 	modified:   src/test/java/com/example/DemoApplicationTests.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -9,4 +9,15 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	public static double diviser(Integer a, Integer b)
+    {
+    	if (b==0)
+    	{
+    		return 0;
+    	}
+    	
+    	return  a/b;
+    	
+    }
 }
--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,9 +1,15 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,5 +18,34 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+    
+    @Test
+    public void testDivision()
+    {	
+    double res= DemoApplication.diviser(8, 4);
+    assertEquals(res, 2,0);
+    }
+    
+    @Test
+    public void testDivision2()
+    {	
+    double res= DemoApplication.diviser(12, 2);
+    assertEquals(res, 6,0);
+    }
+    
+    @Test
+    public void testDivision3()
+    {	
+    double res= DemoApplication.diviser(24, 0);
+    assertEquals(res, 0,0);
+    }
 
+    @Test
+    @Ignore
+    public void testDivision4()
+    {
+    double res= DemoApplication.diviser(12, 3);
+    assertEquals(res, 5,0);
+    }
+	
 }
