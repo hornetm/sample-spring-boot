ff895fe9cb6aae1eb2066bf86380b0a9e82b56f2
inow <mvincent@sigma.fr>
2018-03-22 09:25:23 +0100

    modified:   bin/com/example/DemoApplication.class 	modified:   src/main/java/com/example/DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -10,6 +10,13 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
+	/**
+	 * C'est l méthode qui sert à diviser
+	 * @param a dividende
+	 * @param b diviseur
+	 * @return return 0 si le diviseur est 0 et return le résultat de la division
+	 */
+	
 	public static double diviser(Integer a, Integer b)
     {
     	if (b==0)
